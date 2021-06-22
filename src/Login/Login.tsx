aef280da6ff257787bccd4baa567e8406fcbb2ab
Aaryan Kapoor <hi.aaryankapoor@gmail.com>
2021-06-22 23:26:42 +0530

    update login page 🎱

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -5,6 +5,7 @@ import valogo from '../Assets/valogo.png';
 function Login() {
 
 	const Y = new Date().getFullYear();
+	const pjson = require('../../package.json');
 
 	return (
 		<div className="h-screen bg-black text-white">
@@ -15,7 +16,7 @@ function Login() {
 
 			<div className="hidden lg:flex items-center absolute bottom-0 right-0 px-10 py-5 text-xs">
 				<IconActivity className="w-4 mr-3 text-yellow-400" stroke={2} />
-				<span className="text-xs">x-track . powered by foxsys-xyz, {Y}.</span>
+				<span className="text-xs">x-track {pjson.version}.</span>
 			</div>
 
 			<div className="container h-full mx-auto flex justify-center items-center">
@@ -29,7 +30,7 @@ function Login() {
 						<div className="w-full">
 							<button className="text-sm justify-center lg:text-base w-full flex items-center focus:outline-none px-4 py-2 rounded-full focus:shadow-outline bg-blue-600 hover:bg-blue-700 transition duration-500" placeholder="username">
 								sign in with foxsys-xyz
-                <IconLockOpen className="inline-block w-6 ml-3" stroke={2} />
+								<IconLockOpen className="inline-block w-6 ml-3" stroke={2} />
 							</button>
 						</div>
 					</div>
